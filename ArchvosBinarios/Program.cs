3e65867cb080c4627d83c9b7b58beb2ac6270dca
Yahirs19 <L21212055@tectijuana.edu.mx>
2022-06-09 10:20:28 -0700

    Update

---
+++ ArchvosBinarios/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchvosBinarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
